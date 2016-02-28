391920303d6e08bd923570f6396f4ca5aa438bc4
trout314 <atrout@chatham.edu>
2016-02-28 17:32:03 -0500

    cleanup

--- source/app.d
+++ source/app.d
@@ -3,13 +3,12 @@ void main()
     import std.stdio : writeln;
     writeln("Entering main...");
     
-    import simplex; 
+    import simplex : test; 
     test();
     
     import std.range : iota;
     iota(5).writeln;
     
-    import rational_extension_vector;
-
+    import rational_extension_vector : simplexVecs;
     writeln(simplexVecs!10());
 }
