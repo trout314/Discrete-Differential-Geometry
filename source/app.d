9621bed39d71985142e8799936e396d40874e8a1
trout314 <atrout@chatham.edu>
2016-05-17 21:24:03 -0400

    testing...

--- source/app.d
+++ source/app.d
@@ -10,13 +10,13 @@ void main()
     iota(5).writeln;
     
     import rational_extension_vector : simplexVecs;
-    auto sVecs = simplexVecs!4(); 
+    immutable sVecs = simplexVecs!4(); 
     foreach(sVec; sVecs)
     {
         writeln(sVec);
     }
     
-    import std.rational;
+    import std.rational : rational;
     auto a = rational(2,4);
     writeln(a);
     writeln(typeof(sVecs).stringof);
