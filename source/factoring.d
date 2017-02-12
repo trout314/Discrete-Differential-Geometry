8627e3c191a1b74fa5c5caf08f037796227c0834
trout314 <atrout@chatham.edu>
2017-02-11 21:52:38 -0500

    more documentation unittests

--- source/factoring.d
+++ source/factoring.d
@@ -1,47 +1,5 @@
-version(unittest)
-{
-    import std.range.primitives : isInputRange;
-    import std.range : array;
-    import std.traits : ReturnType;
-    import std.typecons : tuple;
-    import std.algorithm : reduce, equal;
-}
-
 /++
-Returns an input range that enumerates the prime factors of num. This range has the same content as the array returned by `primeFactors(num)`.
-
-Params:
-    num =   a positive integer
-
-Returns:
-    an input range containing the prime factors of num.
-
-See_Also:
-    `primeFactors`
-+/
-auto primeFactorsRange(int num) pure nothrow @nogc
-{
-    assert(num > 0);
-
-    return PrimeFactorsRange(num);
-}
-
-///
-unittest
-{
-    static assert(isInputRange!(ReturnType!primeFactorsRange));
-
-    assert(primeFactorsRange(20).array == [2, 2, 5]);
-    assert(primeFactors(20) == [2, 2, 5]);
-
-    assert(primeFactorsRange(1).empty);
-
-    // This should work, but eats all memory then crashes. 
-    // static assert(primeFactorsRange(1).empty);
-}
-
-/++
-Returns a newly allocated dynamic array containing the prime factors of `num`. This array had the same content as `primeFactorsRange(num).array`.
+Returns a newly allocated dynamic array containing the prime factors of `num`. This array has the same content as `primeFactorsRange(num).array`.
 
 Params:
     num =   a positive integer
@@ -90,30 +48,6 @@ unittest
     // assert(primeFactorsRange(30).array == [2, 3, 5]);
 }
 
-unittest
-{
-    static assert(primeFactors(1) == []);
-
-    enum testFactorLists = tuple([2], [3], [7919], [2, 7], [3, 11],[5, 7529],
-            [2, 2], [13, 13], [7529, 7529], [2, 3, 5, 7, 11, 13, 17],
-            [41, 41, 53, 67], [2, 3, 5, 11, 101, 101], [2, 2, 2, 11, 17], [2,
-            2, 2, 11, 11, 17], [2, 11, 4259, 4259], [2, 2, 2, 2, 2, 2, 2, 2],
-            [5, 5, 5, 5, 5, 5, 5], [3, 3, 3, 3, 7, 7, 7]);
-
-    foreach (factors; testFactorLists)
-    {
-        immutable number = 1.reduce!product(factors);
-        assert(number.primeFactors.equal(factors));
-        static assert(number.primeFactors.equal(factors));
-
-        // TO DO: Why is this so slow?    
-        // assert(number.primeFactorsRange.equal(factors));
-
-        // TO DO: Why is this slow and use so much memory?        
-        // static assert(number.primeFactorsRange.equal(factors));
-    }
-}
-
 /++
 Returns a newly allocated dynamic array containing (in increasing order) those primes which appear to an odd power in the prime-factorization of `num`. The product of these primes is `squareFreePart(num)` and is the largest divisor of `num` not divisible by any square. This is also what's left under the the root after "simplifying" the square root of `num` by moving any squares outside the root.
 
@@ -142,9 +76,22 @@ int[] squareFreePrimeFactors(int num)
 unittest
 {
     assert(squareFreePrimeFactors(1) == []);
-    static assert(squareFreePrimeFactors(1) == []);
+    assert(squareFreePrimeFactors(11 * 11) == []);
+    assert(squareFreePrimeFactors(2 * 2 * 5 * 5 * 5 * 7 * 11) == [5, 7, 11]);
 }
 
+/++
+Returns a newly allocated dynamic array containing (in increasing order) the prime factors of the largest square divisor of `num`. The product of the primes in this array will be `squarePart(num)`.
+
+Params:
+    num =   a positive integer
+
+Returns:
+    a dynamic array of ints containing (in increasing order) the prime factors of the largest square divisor of `num`
+
+See_Also:
+    `squarePart`
++/
 int[] squarePrimeFactors(int num)
 {
     assert(num > 0);
@@ -155,13 +102,26 @@ int[] squarePrimeFactors(int num)
     return num.primeFactors.setDifference(num.squareFreePrimeFactors).array;
 }
 
+///
 unittest
 {
     assert(squarePrimeFactors(1) == []);
-    static assert(squarePrimeFactors(1) == []);
-
+    assert(squarePrimeFactors(11 * 11) == [11, 11]);
+    assert(squarePrimeFactors(2 * 2 * 5 * 5 * 5 * 7 * 11) == [2, 2, 5, 5]);
 }
 
+/++
+Returns a newly allocated dynamic array containing (in increasing order) the prime factors of the square root of the largest square divisor of `num`. The product of the primes in this array will be `sqrtSquarePart(num)`.
+
+Params:
+    num =   a positive integer
+
+Returns:
+    a dynamic array of ints containing (in increasing order) the prime factors of the square root of the largest square divisor of `num`
+
+See_Also:
+    `sqrtSquarePart`
++/
 int[] sqrtSquarePrimeFactors(int num)
 {
     assert(num > 0);
@@ -170,10 +130,12 @@ int[] sqrtSquarePrimeFactors(int num)
     return num.squarePrimeFactors.stride(2).array;
 }
 
+///
 unittest
 {
     assert(sqrtSquarePrimeFactors(1) == []);
-    static assert(sqrtSquarePrimeFactors(1) == []);
+    assert(sqrtSquarePrimeFactors(11 * 11) == [11]);
+    assert(sqrtSquarePrimeFactors(2 * 2 * 5 * 5 * 5 * 7 * 11) == [2, 5]);
 }
 
 int squareFreePart(int num)
@@ -203,17 +165,37 @@ int sqrtSquarePart(int num)
     return 1.reduce!product(num.sqrtSquarePrimeFactors);
 }
 
-// Test all functions together on range of inputs (runtime checks only)
+/// Some additional tests
 unittest
 {
-    import std.algorithm : sort, reduce, equal;
-    import std.range : chain;
-    import std.typecons : staticIota;
+    static assert(primeFactors(1) == []);
+    static assert(squareFreePrimeFactors(1) == []);
+    static assert(squarePrimeFactors(1) == []);
+    static assert(sqrtSquarePrimeFactors(1) == []);
+
+    enum testFactorLists = tuple([2], [3], [7919], [2, 7], [3, 11],[5, 7529],
+            [2, 2], [13, 13], [7529, 7529], [2, 3, 5, 7, 11, 13, 17],
+            [41, 41, 53, 67], [2, 3, 5, 11, 101, 101], [2, 2, 2, 11, 17], [2,
+            2, 2, 11, 11, 17], [2, 11, 4259, 4259], [2, 2, 2, 2, 2, 2, 2, 2],
+            [5, 5, 5, 5, 5, 5, 5], [3, 3, 3, 3, 7, 7, 7]);
+
+    foreach (factors; testFactorLists)
+    {
+        immutable number = 1.reduce!product(factors);
+        assert(number.primeFactors.equal(factors));
+        static assert(number.primeFactors.equal(factors));
+
+        // TO DO: Why is this so slow?    
+        // assert(number.primeFactorsRange.equal(factors));
+
+        // TO DO: Why is this slow and use so much memory?        
+        // static assert(number.primeFactorsRange.equal(factors));
+    }
 
     immutable maxRuntimeTest = 10_000;
     foreach (n; 1 .. maxRuntimeTest + 1)
     {
-        assert(sort(chain(n.squareFreePrimeFactors, n.squarePrimeFactors)).equal(n.primeFactors));
+        assert(merge(n.squareFreePrimeFactors, n.squarePrimeFactors).equal(n.primeFactors));
         assert(n == n.squarePart * n.squareFreePart);
         assert(n == n.sqrtSquarePart * n.sqrtSquarePart * n.squareFreePart);
     }
@@ -221,12 +203,46 @@ unittest
     enum maxCompiletimeTest = 100;
     foreach (n; staticIota!(1, maxCompiletimeTest))
     {
-        static assert(sort(chain(n.squareFreePrimeFactors,
-            n.squarePrimeFactors)).equal(n.primeFactors));
+        static assert(merge(n.squareFreePrimeFactors, n.squarePrimeFactors).equal(n.primeFactors));
         static assert(n == n.squarePart * n.squareFreePart);
         static assert(n == n.sqrtSquarePart * n.sqrtSquarePart * n.squareFreePart);
     }
 }
+
+
+/++
+Returns an input range that enumerates the prime factors of num. This range has the same content as the array returned by `primeFactors(num)`.
+
+Params:
+    num =   a positive integer
+
+Returns:
+    an input range containing the prime factors of num.
+
+See_Also:
+    `primeFactors`
++/
+auto primeFactorsRange(int num) pure nothrow @nogc
+{
+    assert(num > 0);
+
+    return PrimeFactorsRange(num);
+}
+
+///
+unittest
+{
+    static assert(isInputRange!(ReturnType!primeFactorsRange));
+
+    assert(primeFactorsRange(20).array == [2, 2, 5]);
+    assert(primeFactors(20) == [2, 2, 5]);
+
+    assert(primeFactorsRange(1).empty);
+
+    // This should work, but eats all memory then crashes. 
+    // static assert(primeFactorsRange(1).empty);
+}
+
 
 private:
 
@@ -284,4 +300,14 @@ struct PrimeFactorsRange
         remainingPart /= currentPart;
         currentPart = remainingPart.nextFactorFrom(currentPart);
     }
+}
+
+version(unittest)
+{
+    import std.algorithm : equal, reduce, sort;
+    import std.algorithm.setops : merge;
+    import std.range : array;
+    import std.range.primitives : isInputRange;
+    import std.traits : ReturnType;
+    import std.typecons : staticIota, tuple;
 }
