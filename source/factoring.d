dad38f40f5d08f933d2a2ee6081b698468c4052d
trout314 <atrout@chatham.edu>
2017-02-13 19:44:38 -0500

    more organizing, document unittests

--- source/factoring.d
+++ source/factoring.d
@@ -2,7 +2,7 @@ version (unittest)
 {
     import std.algorithm : equal, reduce, sort;
     import std.range : array, isForwardRange, chain;
-    import std.traits : ReturnType;
+    import std.traits : ReturnType, hasFunctionAttributes;
     import std.typecons : staticIota, tuple;
 }
 
@@ -15,7 +15,7 @@ Params:
 Returns:
     a forward range of the prime factors of num in increasing order.
 +/
-auto primeFactors(int num) pure nothrow @nogc @safe
+auto primeFactors(int num)
 {
     assert(num > 0);
     return PrimeFactorsRange(num);
@@ -25,11 +25,15 @@ auto primeFactors(int num) pure nothrow @nogc @safe
 unittest
 {
     static assert(isForwardRange!(ReturnType!primeFactors));
+    static assert(hasFunctionAttributes!(primeFactors, "pure", "nothrow", "@nogc", "@safe"));
 
     assert(primeFactors(1).array == []);
-    assert(primeFactors(30).array == [2, 3, 5]);
-    assert(primeFactors(360).array == [2, 2, 2, 3, 3, 5]);
+    assert(primeFactors(2 * 3 * 5).array == [2, 3, 5]);
+    assert(primeFactors(2 * 2 * 2 * 3 * 3 * 5).array == [2, 2, 2, 3, 3, 5]);
     assert(primeFactors(1223).array == [1223]);
+
+    // Everything works at compile-time too
+    static assert(primeFactors(24).array == [2, 2, 2, 3]);
 }
 
 /++
@@ -44,7 +48,7 @@ Returns:
 See_Also:
     `squareFreePart`
 +/
-auto squareFreePrimeFactors(int num) pure nothrow @nogc @safe
+auto squareFreePrimeFactors(int num)
 {
     assert(num > 0);
 
@@ -63,6 +67,9 @@ unittest
     assert(squareFreePrimeFactors(1).array == []);
     assert(squareFreePrimeFactors(11 * 11).array == []);
     assert(squareFreePrimeFactors(2 * 2 * 5 * 5 * 5 * 7 * 11).array == [5, 7, 11]);
+    static assert(squareFreePrimeFactors(24).array == [2, 3]);
+
+    static assert(hasFunctionAttributes!(squareFreePrimeFactors, "pure", "nothrow", "@nogc", "@safe"));
 }
 
 /++
@@ -77,7 +84,7 @@ Returns:
 See_Also:
     `squarePart`
 +/
-auto squarePrimeFactors(int num) pure nothrow @nogc @safe
+auto squarePrimeFactors(int num)
 {
     assert(num > 0);
 
@@ -92,6 +99,9 @@ unittest
     assert(squarePrimeFactors(1).array == []);
     assert(squarePrimeFactors(11 * 11).array == [11, 11]);
     assert(squarePrimeFactors(2 * 2 * 5 * 5 * 5 * 7 * 11).array == [2, 2, 5, 5]);
+    static assert(squarePrimeFactors(24).array == [2, 2]);
+
+    static assert(hasFunctionAttributes!(squarePrimeFactors, "pure", "nothrow", "@nogc", "@safe"));
 }
 
 /++
@@ -106,7 +116,7 @@ Returns:
 See_Also:
     `sqrtSquarePart`
 +/
-auto sqrtSquarePrimeFactors(int num) pure nothrow @nogc @safe
+auto sqrtSquarePrimeFactors(int num)
 {
     assert(num > 0);
     import std.range : stride;
@@ -120,6 +130,10 @@ unittest
     assert(sqrtSquarePrimeFactors(1).array == []);
     assert(sqrtSquarePrimeFactors(11 * 11).array == [11]);
     assert(sqrtSquarePrimeFactors(2 * 2 * 5 * 5 * 5 * 7 * 11).array == [2, 5]);
+
+    static assert(sqrtSquarePrimeFactors(24).array == [2]);
+
+    static assert(hasFunctionAttributes!(sqrtSquarePrimeFactors, "pure", "nothrow", "@nogc", "@safe"));
 }
 
 /++
@@ -149,6 +163,9 @@ unittest
     assert(squareFreePart(1) == 1);
     assert(squareFreePart(11 * 11) == 1);
     assert(squareFreePart(2 * 2 * 5 * 5 * 5 * 7 * 11) == 5 * 7 * 11);
+    static assert(squareFreePart(24) == 2 * 3);
+
+    static assert(hasFunctionAttributes!(squareFreePart, "pure", "nothrow", "@nogc", "@safe"));    
 }
 
 /++
@@ -178,6 +195,9 @@ unittest
     assert(squarePart(1) == 1);
     assert(squarePart(11 * 11) == 11 * 11);
     assert(squarePart(2 * 2 * 5 * 5 * 5 * 7 * 11) == 2 * 2 * 5 * 5);
+    static assert(squarePart(24) == 2 * 2);
+
+    static assert(hasFunctionAttributes!(squarePart, "pure", "nothrow", "@nogc", "@safe"));    
 }
 
 /++
@@ -207,8 +227,10 @@ unittest
     assert(sqrtSquarePart(1) == 1);
     assert(sqrtSquarePart(11 * 11) == 11);
     assert(sqrtSquarePart(2 * 2 * 5 * 5 * 5 * 7 * 11) == 2 * 5);
-}
+    static assert(sqrtSquarePart(24) == 2);
 
+    static assert(hasFunctionAttributes!(sqrtSquarePart, "pure", "nothrow", "@nogc", "@safe"));    
+}
 
 // Some additional tests
 unittest
@@ -226,15 +248,19 @@ unittest
 
     foreach (factors; testFactorLists)
     {
+        // get the number n whose prime factorization is the given factor list 
         immutable n = 1.reduce!((a, b) => a * b)(factors);
+
         assert(n.primeFactors.equal(factors));
         static assert(n.primeFactors.equal(factors));
 
-        assert(sort(chain(n.squareFreePrimeFactors, n.squarePrimeFactors).array).equal(n.primeFactors));
+        auto allPrimeFactors = sort(chain(n.squareFreePrimeFactors, n.squarePrimeFactors).array);
+        assert(allPrimeFactors.equal(factors));
         assert(n == n.squarePart * n.squareFreePart);
         assert(n == n.sqrtSquarePart * n.sqrtSquarePart * n.squareFreePart);
 
-        static assert(sort(chain(n.squareFreePrimeFactors, n.squarePrimeFactors).array).equal(n.primeFactors));
+        enum allPrimeFactors_ = sort(chain(n.squareFreePrimeFactors, n.squarePrimeFactors).array);
+        static assert(allPrimeFactors_.equal(factors));
         static assert(n == n.squarePart * n.squareFreePart);
         static assert(n == n.sqrtSquarePart * n.sqrtSquarePart * n.squareFreePart);
     }
@@ -243,7 +269,8 @@ unittest
     foreach (immutable n; 1 .. maxRuntimeTest + 1)
     {
         assert(1.reduce!((a,b) => a*b)(n.primeFactors) == n);
-        assert(sort(chain(n.squareFreePrimeFactors, n.squarePrimeFactors).array).equal(n.primeFactors));
+        auto allPrimeFactors = sort(chain(n.squareFreePrimeFactors, n.squarePrimeFactors).array);
+        assert(allPrimeFactors.equal(n.primeFactors));
         assert(n == n.squarePart * n.squareFreePart);
         assert(n == n.sqrtSquarePart * n.sqrtSquarePart * n.squareFreePart);
     }
@@ -252,13 +279,12 @@ unittest
     foreach (immutable n; staticIota!(1, maxCompiletimeTest))
     {
         static assert(1.reduce!((a,b) => a*b)(n.primeFactors) == n);
-        static assert(sort(chain(n.squareFreePrimeFactors, n.squarePrimeFactors).array).equal(n.primeFactors));
+        enum allPrimeFactors = sort(chain(n.squareFreePrimeFactors, n.squarePrimeFactors).array);        
+        static assert(allPrimeFactors.equal(n.primeFactors));
         static assert(n == n.squarePart * n.squareFreePart);
         static assert(n == n.sqrtSquarePart * n.sqrtSquarePart * n.squareFreePart);
     }
 }
-
-private:
 
 /++
 Returns the lowest (non-trivial if possible) factor of `num`. If `num == 1` then the function returns `1` otherwise it returns the smallest factor of `num` greater than one.
@@ -269,7 +295,7 @@ Params:
 Returns:
     `int` which is lowest non-trivial factor of `num` if such exists, otherwise 1 if `num` is 1.
 +/
-int lowestFactor(int num) pure nothrow @nogc @safe
+auto lowestFactor(int num)
 {
     assert(num > 0);
 
@@ -295,6 +321,7 @@ int lowestFactor(int num) pure nothrow @nogc @safe
     return possibleFactor;
 }
 
+///
 unittest
 {
     assert(lowestFactor(1) == 1);
@@ -304,7 +331,13 @@ unittest
     assert(lowestFactor(11) == 11);
     assert(lowestFactor(2 * 2 * 5 * 11) == 2);
     assert(lowestFactor(13 * 13) == 13);
+
+    static assert(lowestFactor(2*3) == 2);
+
+    static assert(hasFunctionAttributes!(lowestFactor, "pure", "nothrow", "@nogc", "@safe"));    
 }
+
+private:
 
 struct PrimeFactorsRange
 {
@@ -345,7 +378,8 @@ struct PrimeFactorsRange
         factor = number.lowestFactor;
     }
 
-    @property PrimeFactorsRange save() pure nothrow @nogc @safe const {
+    @property PrimeFactorsRange save() pure nothrow @nogc @safe const
+    {
         return this;
     }
 }
