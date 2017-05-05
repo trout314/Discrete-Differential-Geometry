c7e419fe0bc29f3be0fb4025794da01c77586efc
trout314 <atrout@chatham.edu>
2017-05-05 13:05:56 -0400

    basic simplicial complex functionality

--- source/simplicial_complex.d
+++ source/simplicial_complex.d
@@ -1,11 +1,16 @@
 import simplex : Simplex, simplex;
 
+version(unittest)
+{
+    import std.exception : assertThrown;
+    import std.stdio : writeln;
+}
 
 /// A simplicial complex type ... yada yada
 struct SimplicialComplex
 {
-    // Inserts a facet into the simplicial complex. T
-    void insertFacet(int[] vertices) pure nothrow @safe @nogc
+    // Inserts a facet into the simplicial complex.
+    void insertFacet(int[] vertices) pure nothrow @safe
     in
     {
         import std.algorithm : isSorted, findAdjacent;
@@ -14,14 +19,44 @@ struct SimplicialComplex
     }
     body
     {
+        import std.range.primitives : walkLength;
+        immutable numVerts = vertices.walkLength;
+        auto ptr = numVerts in facets;
+        if (!ptr)
+        {
+            facets[numVerts] ~= vertices;
+        }
+        else
+        {
+            import std.algorithm : canFind;
+            assert(!facets[numVerts].canFind(vertices), "facet must not already exist");
+            *ptr ~= vertices;
+        }
     }
+
+    // Returns the number of facets 
+
+
     private:
+    // Facets indexed by number of vertices
+    int[][][size_t] facets;
     
 }
 
-pure nothrow @safe unittest
+unittest
 {
     SimplicialComplex sc;
-    sc.insertFacet([1,3,5]);
-    sc.insertFacet([1,3,3]);
+
+    // insert a 2-simplex [1,5,7]
+    sc.insertFacet([1,5,7]);
+    sc.insertFacet([2,3,4]);
+    sc.insertFacet([2,3,4]);
+
+    writeln(sc.facets);
+
+    // vertices in an inserted facet must be sorted
+    assertThrown!Error(sc.insertFacet([1,5,2,3]));
+
+    // vertices may not be repeated
+    assertThrown!Error(sc.insertFacet([1,3,3]));
 }
