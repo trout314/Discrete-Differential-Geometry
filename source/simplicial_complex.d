cf77387ad398552eb8e37e54e96710fab80d9be5
trout314 <atrout@chatham.edu>
2017-05-06 14:36:11 -0400

    star and contains work...

--- source/simplicial_complex.d
+++ source/simplicial_complex.d
@@ -1,36 +1,44 @@
-import simplex : Simplex, simplex;
-
 version(unittest)
 {
     import std.exception : assertThrown;
     import std.stdio : writeln;
 }
 
+bool hasFace(Simplex s1, Simplex s2)
+{
+    import std.algorithm : all, canFind;
+    return s2.all!(vertex => s1.canFind(vertex));
+}
+
+private alias Simplex = int[];
+
 /// A simplicial complex type ... yada yada
 struct SimplicialComplex
 {
     // Inserts a facet into the simplicial complex.
-    void insertFacet(int[] vertices) pure nothrow @safe
+    void insertFacet(Simplex facet) pure nothrow @safe
     in
     {
         import std.algorithm : isSorted, findAdjacent;
-        assert(vertices.isSorted, "vertices must be sorted");
-        assert(vertices.findAdjacent.length == 0, "repeated vertices not allowed");
+        assert(facet.isSorted, "vertices must be sorted");
+        assert(facet.findAdjacent.length == 0,
+            "repeated vertices not allowed");
     }
     body
     {
         import std.range.primitives : walkLength;
-        immutable numVerts = vertices.walkLength;
+        immutable numVerts = facet.walkLength;
         auto ptr = numVerts in facets_;
         if (ptr is null)
         {
-            facets_[numVerts] ~= vertices;
+            facets_[numVerts] ~= facet;
         }
         else
         {
             import std.algorithm : canFind;
-            assert(!facets_[numVerts].canFind(vertices), "facet must not already exist");
-            *ptr ~= vertices;
+            assert(!facets_[numVerts].canFind(facet),
+                "facet must not already exist");
+            *ptr ~= facet;
         }
 
         import std.algorithm : sort;
@@ -39,7 +47,8 @@ struct SimplicialComplex
         // TO DO: Why doesn't sort(*ptr) work?
     }
 
-    // Returns the facets of the simplicial complex. These are simplicies that are not the face of another simplex.
+    // Returns the facets of the simplicial complex.
+    // These are simplicies that are not the face of another simplex.
     auto facets()
     {
         import std.range : array;
@@ -58,18 +67,27 @@ struct SimplicialComplex
 
     // Returns the link of the given simplex as a list of facets
     // in same order as they appear in facets()
-    auto link(int[] simplex)
+    auto link(Simplex simplex)
     {
     }
 
-    // Returns the closure of the star of the given simplex. 
-    auto star(int[] simplex)
+    // Returns the star of the given simplex as a list of facets
+    // in same order as they appear in facets() 
+    auto star(Simplex simplex)
     {
+        import std.algorithm : filter;
+        return facets.filter!(facet => facet.hasFace(simplex));
+    }
+
+    bool contains(Simplex simplex)
+    {
+        import std.algorithm : any;
+        return facets.any!(facet => facet.hasFace(simplex));
     }
 
     private:
-    // Facets indexed by number of vertices
-    int[][][size_t] facets_;
+    // Lists of facets, indexed by number of vertices in the facet
+    Simplex[][size_t] facets_;
     
 }
 
@@ -83,6 +101,13 @@ unittest
     sc.insertFacet([7,9]);
 
     assert(sc.facets == [[7, 9], [1,5,7], [1,5,8]]);
+
+    // can check for the presence of simplices
+    assert(sc.contains([7,9]));
+    assert(!sc.contains([1,5,6]));
+    assert(sc.contains([1,5]));
+    assert(sc.contains([7]));
+    assert(!sc.contains([8, 9]));
 
     // can get the number of facets
     assert(sc.numFacets == 3);
