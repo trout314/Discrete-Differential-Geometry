f022cf57daeed1cad3f04749d415b318dcf1e305
trout314 <atrout@chatham.edu>
2017-06-21 15:18:59 -0400

    restructured REVector

--- source/rational_extension_vector.d
+++ source/rational_extension_vector.d
@@ -18,7 +18,8 @@ Returns:
 */
 auto simplexPoints(int dim)() pure nothrow @nogc @safe
 {
-    alias Vec = REVector!dim;
+    alias Vec = REVector!(
+        iota(1, dim + 1).map!(k => squareFreePart(k * (k + 1) / 2)).array);
     return iota(0, dim + 1).map!(k => Vec(simplexCoefs(dim)[k]));
 }
 
@@ -32,21 +33,40 @@ pure nothrow @safe unittest
     v3 = (1/2, (1/6)√3, (1/3)√6)
     */
 
-    alias Vec = reVector;
     alias r = rational;
-    auto pts = [Vec(  r(0),   r(0),   r(0)),
-                Vec(  r(1),   r(0),   r(0)),
-                Vec(r(1,2), r(1,2),   r(0)),
-                Vec(r(1,2), r(1,6), r(1,3))];
-
+    enum roots = [1,3,6];
+    alias vec = reVector!roots;
+    auto pts = [vec(  r(0),   r(0),   r(0)),
+                vec(  r(1),   r(0),   r(0)),
+                vec(r(1,2), r(1,2),   r(0)),
+                vec(r(1,2), r(1,6), r(1,3))];
+    
     assert(simplexPoints!3.equal(pts));
     assert(simplexPoints!3.all!(v => v.roots == [1,3,6]));
-
     static assert(isForwardRange!(ReturnType!(simplexPoints!3)));
 }
 
-struct REVector(int dim)
+struct REVector(int[] theRoots)
 {
+    
+
+    // TO DO: Add some constraints to this template?
+    this(T)(T coefs)
+    {
+        assert(coefs.walkLength == dim);
+        copy(coefs, rationalCoefs[]);
+    }
+
+    static this()
+    {
+        roots_[] = theRoots[];
+    }
+
+    auto roots() const
+    {
+        return roots_[];
+    }
+    
     ///
     string toString()
     {
@@ -62,19 +82,19 @@ struct REVector(int dim)
             {
                 result ~= "1";
             }
-            else if (roots_[i] == 1)
+            else if ((roots_[i] == 1) && (coef != 1))
             {
                 result ~= to!string(coef);
             }
-            else
-            {
-                result ~= "(" ~ to!string(coef) ~ ")";
-            }
-
-            // Append root portion if needed
-            if ((roots_[i] > 1) && (coef != 1) && (coef != 0))
+            else if ((roots_[i] != 1) && (coef == 1))
             {
                 result ~= x"E2 88 9A"; // square root symbol UTF-8 (hex)
+                result ~= to!string(roots_[i]);
+            }
+            else if ((roots_[i] != 1) && (coef != 1))
+            {
+                result ~= "(" ~ to!string(coef) ~ ")";            
+                result ~= x"E2 88 9A";
                 result ~= to!string(roots_[i]);
             }
 
@@ -87,36 +107,19 @@ struct REVector(int dim)
         result ~= ")";
         return result;
     }
-
-    // TO DO: Add some constraints to this template?
-    this(T)(T coefs)
-    {
-        assert(coefs.walkLength == dim);
-        copy(coefs, rationalCoefs[]);
-    }
-
-    static this()
-    {
-        foreach (index, root; enumerate(simplexRoots(dim)))
-        {
-            roots_[index] = root;
-        }
-    }
-
-    @property auto roots() const
-    {
-        return roots_;
-    }
-
 private:
+    static immutable dim = theRoots.walkLength;
     static immutable int[dim] roots_;
     Rational!int[dim] rationalCoefs;
 }
 
-auto reVector(T, size_t dim)(Rational!T[dim] coefficients...)
+template reVector(int[] roots)
 {
-    alias r = Rational!T;
-    return REVector!dim(coefficients[]);
+    auto reVector(T, size_t dim)(Rational!T[dim] coefficients...)
+    {
+        static assert(coefficients.length == roots.walkLength);
+        return REVector!roots(coefficients[]);
+    }
 }
 
 auto dotProduct(size_t dim)(REVector!dim v0, REVector!dim v1)
@@ -169,5 +172,24 @@ auto simplexCoefs(int dim)
 
     return iota(0, dim + 1).map!(pointIndex => iota(1, dim + 1)
             .map!(basisIndex => computeCoef(pointIndex, basisIndex)));
+
+}
+
+unittest
+{
+    alias r = rational;
+
+    assert(REVector!([1,3,6])([r(1, 1), r(3, 7), r(11, 2)]).toString ==
+        "(1, (3/7)√3, (11/2)√6)");
+
+    assert(REVector!([1,3,6])([r(1, 2), r(0, 1), r(1, 1)]).toString ==
+        "(1/2, 0, √6)");
+
+    auto v = REVector!([1,3,6])([r(1,2), r(1,3), r(0,1)]);
+
+
+    import std.stdio : writeln;
+    v.writeln;
+    v.roots.writeln;
 
 }
