01505f98d40fd9d19676b45ff41e1ccc128b6735
trout314 <atrout@chatham.edu>
2018-08-05 14:48:25 -0400

    try out new sampler

--- source/app.d
+++ source/app.d
@@ -3,10 +3,16 @@ else
 {
     void main()
     {
-        import sampler : sampleOld;
         // import gperftools_d.profiler;
         // ProfilerStart();
-        sampleOld;
+        // sampleOld;
         // ProfilerStop();
+
+        import sampler;
+
+        import manifold : Manifold, standardSphereFacets;
+        auto m = Manifold!3(standardSphereFacets(3));
+        auto s = Sampler!(int, 3)(m);
+        s.sample;
     }
 }
