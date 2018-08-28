eb48490039d8756b158f9adc945a781780313f6f
trout314 <atrout@chatham.edu>
2018-08-27 21:38:35 -0400

    degreeVariance unittests

--- source/manifold.d
+++ source/manifold.d
@@ -13,6 +13,8 @@ import utility : binomial, isSubsetOf, SmallMap, StackArray, staticIota, subsets
     throwsWithMsg, toImmutStaticArray, toStackArray, toStaticArray;
 import std.stdio : File;
 import std.typecons : Flag, No, Yes;
+import std.math : approxEqual;
+
 //dfmt off
 /*******************************************************************************
 Manifold type... TO DO: More info here.
@@ -969,6 +971,33 @@ real degreeVariance(Vertex, int mfdDim)(const ref Manifold!(mfdDim, Vertex) mfd,
     immutable meanSqrDeg = mfd.totalSquareDegree(dim) / real(mfd.fVector[dim]);
     return meanSqrDeg - meanDeg^^2;
 }
+///
+@Name("degreeVariance") unittest
+{
+    foreach(dim; staticIota!(1, 8))
+    {
+        auto m = standardSphere!dim;
+        assert(dim.iota.all!(d => m.degreeVariance(d) == 0));
+    }
+
+    auto pyramid = Manifold!2(
+        [[0,1,2], [0,2,3], [0,1,3], [1,2,4], [2,3,4], [1,3,4]]);
+
+
+    // pyramid has two vertices with deg(v)=3 and three vertices with deg(v)=4
+    real md0 = pyramid.meanDegree(0);
+    real epsilon = 1e-20;
+    assert(md0.approxEqual((2 * 3 + 3 * 4)/5.0, epsilon));
+    assert(pyramid.meanDegree(1).approxEqual(2.0, epsilon));
+    assert(pyramid.meanDegree(2).approxEqual(1.0, epsilon));
+
+    assert(pyramid.degreeVariance(0)
+        .approxEqual((2 * (3 - md0)^^2 + 3 * (4 - md0)^^2)/5.0, epsilon));
+
+    assert(pyramid.degreeVariance(1).approxEqual(0.0, epsilon));
+    assert(pyramid.degreeVariance(2).approxEqual(0.0, epsilon));
+
+}
 
 /******************************************************************************
 Does the 'diskIndx'-th hinge move associated at 'hinge'. Must give the
@@ -988,6 +1017,7 @@ if (isInputRange!H && is(ElementType!H : Vertex)
     auto hinge = hinge_.toStaticArray!(dim - 1);
 
     // TO DO: Decide what to do about this magic constant 7
+    // (It comes from nGonTriangs only supporting up to 7-gon.)
     auto linkVertices = linkVertices_.toStaticArray!7;
 
     immutable deg = manifold.degree(hinge_).to!int;
