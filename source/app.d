0f0b34905fda63ecf82a35bfc9fb9724d6697762
trout314 <atrout@chatham.edu>
2018-09-07 18:34:34 -0400

    added crude manifold saving

--- source/app.d
+++ source/app.d
@@ -6,12 +6,18 @@ else
         // import gperftools_d.profiler;
         // ProfilerStart();
 
+        import std.stdio : File;
         import sampler;
-
-        import manifold : standardSphere;
+        import manifold : standardSphere, saveTo;
+        
         auto s = Sampler!(int, 3)(standardSphere!3);
         s.sample;
 
+        auto fileName = "test.dat";
+        s.manifold.saveTo(fileName);
+        auto saveFile = File(fileName, "a");
+        s.report(saveFile);
+    
         // ProfilerStop();
     }
 }
