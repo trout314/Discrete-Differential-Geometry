25329604a7c850c42b4f614d33ad0d762c9b09e4
trout314 <atrout@chatham.edu>
2018-09-26 08:27:24 -0400

    using saveEdgeGraph

--- source/app.d
+++ source/app.d
@@ -8,16 +8,18 @@ else
 
         import std.stdio : File;
         import sampler;
-        import manifold : standardSphere, saveTo;
-        
-        auto s = Sampler!(int, 3)(standardSphere!3);
-        s.sample;
+        import manifold : standardSphere, saveTo, saveEdgeGraphTo, loadManifold;
 
-        auto fileName = "test.dat";
-        s.manifold.saveTo(fileName);
-        auto saveFile = File(fileName, "a");
-        s.report(saveFile);
-    
+        auto m = loadManifold!3("mfd_S3_128000_VGC.dat");
+        m.saveEdgeGraphTo("edge_graph_S3_128000_VGC.dat");
+
+        // auto s = Sampler!(int, 3)(standardSphere!3);
+        // s.sample;
+
+        // auto fileName = "edge_graph_test.dat";
+        // s.manifold.saveEdgeGraphTo(fileName);
+        // auto saveFile = File(fileName, "a");
+   
         // ProfilerStop();
     }
 }
