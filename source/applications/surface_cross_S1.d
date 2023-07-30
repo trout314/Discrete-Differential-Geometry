178fec13f53950b299f0d1a326937c514a4d3ca5
Aaron Trout <atrout@chatham.edu>
2023-07-30 16:15:39 -0400

    loads surface from file

--- source/applications/surface_cross_S1.d
+++ source/applications/surface_cross_S1.d
@@ -1,7 +1,35 @@
 module applications.surface_cross_S1;
 
-version(unittest) {} else {
-void main()
+import std.getopt;
+
+import manifold, utility;
+
+version (unittest) {} else {
+int main(string[] args)
 {
-    
+    string surfaceFileName;
+    auto helpInformation = getopt(args, std.getopt.config.required,
+        "surfaceFile|s", &surfaceFileName);
+
+    if (helpInformation.helpWanted)
+    {
+        // TO DO: some actual help info here!
+        defaultGetoptPrinter("TO DO: Help info",
+            helpInformation.options);
+        return 0; // exit with return code zero (success)
+    }
+
+    auto surface = loadManifold!2(surfaceFileName);
+
+    foreach(vertex; surface.simplices(0))
+    {
+        dump!vertex;
+    }
+
+
+
+
+    dump!surface;
+
+    return 0;
 }}
