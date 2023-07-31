0d8a07d039ae79263fb3239c100937f5c61fc554
Aaron Trout <atrout@chatham.edu>
2023-07-31 09:28:07 -0400

    work on get graph applications

---
+++ source/applications/dual_graph.d
@@ -0,0 +1,35 @@
+module applications.dual_graph;
+
+import std.getopt;
+
+import simplicial_complex, utility;
+
+version (unittest) {} else {
+int main(string[] args)
+{
+    string surfaceFileName;
+    auto helpInformation = getopt(args, std.getopt.config.required,
+        "simplicialComplex|s", &surfaceFileName);
+
+    if (helpInformation.helpWanted)
+    {
+        // TO DO: some actual help info here!
+        defaultGetoptPrinter("TO DO: Help info",
+            helpInformation.options);
+        return 0; // exit with return code zero (success)
+    }
+
+    auto surface = loadSimplicialComplex(surfaceFileName);
+
+    foreach(vertex; surface.simplices(0))
+    {
+        dump!vertex;
+    }
+
+
+
+
+    dump!surface;
+
+    return 0;
+}}
